490f162a2fac83bc658b93f7250bf519c575aee2
Nayekah <katou7.ayaka@gmail.com>
2025-03-18 16:20:46 +0700

    feat: integrate keyboard to kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,21 +1,36 @@
 #include <stdint.h>
 #include "header/cpu/gdt.h"
+#include "header/cpu/portio.h"
 #include "header/text/framebuffer.h"
 #include "header/kernel-entrypoint.h"
+#include "header/interrupt/interrupt.h"
+#include "header/interrupt/idt.h"
+#include "header/driver/keyboard.h"
+#include "header/stdlib/string.h"
+
 
 void kernel_setup(void) {
-    load_gdt(&_gdt_gdtr); // load gdt
-
-    framebuffer_clear(); //framebuffer
-    framebuffer_write(3, 8,  'H', 0, 0xF);
-    framebuffer_write(3, 9,  'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_set_cursor(3, 10);
-
-
-    uint32_t a;
-    uint32_t volatile b = 0x0000BABE;
-    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
-    while (true) b += 1;
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    activate_keyboard_interrupt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+   
+    int row = 0, col = 0;
+    keyboard_state_activate();
+    while (true) {
+        char c;
+        get_keyboard_buffer(&c);
+        if (c) {
+            framebuffer_write(row, col, c, 0xF, 0);
+            if (col >= FRAMEBUFFER_WIDTH) {
+                ++row;
+                col = 0;
+            } else {
+                ++col;
+            }
+           framebuffer_set_cursor(row, col);
+        }
+    }
 }
