1b9630b7d3eb5c85fe4004f2239799a787d81090
BoredAngel <13523085@std.stei.itb.ac.id>
2025-03-19 20:55:23 +0700

    feat: add backspace for keyboard input

--- src/kernel.c
+++ src/kernel.c
@@ -21,7 +21,25 @@ void kernel_setup(void) {
     while (true) {
         char c;
         get_keyboard_buffer(&c);
-        if (c) {
+
+        // backspace  -jibs
+        if (c == '\b') {
+            if (col == 0 && row == 0) { // not sure what to do actually
+                row = 0;
+                col = 0;
+            }
+            else if (col == 0) { // go back one row
+                --row;
+                col = FRAMEBUFFER_WIDTH;
+            } 
+            else { // go back one col
+                --col;
+            }
+            framebuffer_write(row, col, '\0', 0xF, 0);  // replace printed char with \0 which is just empty
+            framebuffer_set_cursor(row, col);
+        }
+        // other char
+        else if (c) {
             framebuffer_write(row, col, c, 0xF, 0);
             if (col >= FRAMEBUFFER_WIDTH) {
                 ++row;
