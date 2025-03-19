e6d924394cbc3c004f525059f008af56029621f8
Nayekah <katou7.ayaka@gmail.com>
2025-03-19 14:27:02 +0700

    feat: add keyboard driver

---
+++ src/comps/driver/keyboard.c
@@ -0,0 +1,55 @@
+#include "header/driver/keyboard.h"
+#include "header/cpu/portio.h"
+#include "header/stdlib/string.h"
+
+static struct KeyboardDriverState keyboard_state = {
+  .keyboard_input_on = false,
+};
+
+const char keyboard_scancode_1_to_ascii_map[256] = {
+      0, 0x1B, '1', '2', '3', '4', '5', '6',  '7', '8', '9',  '0',  '-', '=', '\b', '\t',
+    'q',  'w', 'e', 'r', 't', 'y', 'u', 'i',  'o', 'p', '[',  ']', '\n',   0,  'a',  's',
+    'd',  'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   0, '\\',  'z', 'x',  'c',  'v',
+    'b',  'n', 'm', ',', '.', '/',   0, '*',    0, ' ',   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0, '-',    0,    0,   0,  '+',    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+};
+
+void keyboard_state_activate(void){
+  if(!keyboard_state.keyboard_input_on){
+      keyboard_state.keyboard_input_on = true;
+  }
+}
+
+void keyboard_state_deactivate(void){
+  if(keyboard_state.keyboard_input_on){
+      keyboard_state.keyboard_input_on = false;
+  }
+}
+
+void get_keyboard_buffer(char *buf) {
+  *buf = keyboard_state.keyboard_buffer;
+  keyboard_state.keyboard_buffer = 0x0;
+}
+
+void keyboard_isr(void) {
+
+  uint16_t scancode = in(KEYBOARD_DATA_PORT);
+
+  if(keyboard_state.keyboard_input_on){
+      uint16_t mapped = keyboard_scancode_1_to_ascii_map[scancode];
+      keyboard_state.keyboard_buffer = mapped;
+  }
+  pic_ack(PIC1_OFFSET + IRQ_KEYBOARD);
+}
